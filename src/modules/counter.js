fc0cedb76abeac58230834e51d421c637ccf5e36
piwe2004 <piwe2004@gmail.com>
2021-07-15 14:16:08 +0900

    리덕스 미들웨어 연습

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -7,3 +7,15 @@ export const increase = () => ({type:INCREASE});
 export const decrease = () => ({type:DECREASE});
 
 // 초기값 (상태가 객체가 아니라 그냥 숫자여도 상관없습니다.)
+const initialState = 0;
+
+export default function counter(state = initialState, action){
+    switch (action.type) {
+        case INCREASE:
+            return state + 1;
+        case DECREASE :
+            return state  - 1;
+        default :
+            return state;
+    }
+}
