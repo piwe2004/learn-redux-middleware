bc0592bda82646368fba15df8adcbd334bcb2389
piwe2004 <piwe2004@gmail.com>
2021-07-22 11:13:07 +0900

    리덕스 미들웨어 연습  - redux-thunk 프로미스

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -6,16 +6,25 @@ const DECREASE = 'DECREASE';
 export const increase = () => ({type:INCREASE});
 export const decrease = () => ({type:DECREASE});
 
+// getState를 쓰지 않는다면 굳이 파라미터로 받아올 필요 없습니다.
+export const increaseAsync = () => dispatch => {
+    setTimeout(() => dispatch(increase()), 1000);
+};
+
+export const decreaseAsync = () => dispatch => {
+    setTimeout(() => dispatch(decrease()), 1000);
+};
+
 // 초기값 (상태가 객체가 아니라 그냥 숫자여도 상관없습니다.)
 const initialState = 0;
 
-export default function counter(state = initialState, action){
+export default function counter(state = initialState, action) {
     switch (action.type) {
         case INCREASE:
             return state + 1;
-        case DECREASE :
-            return state  - 1;
-        default :
+        case DECREASE:
+            return state - 1;
+        default:
             return state;
     }
 }
